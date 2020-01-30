4e56944f00c6e590a563dfcd2fb7d8a11abac7f0
pop stefan <stefanpop999@gmail.com>
2020-01-30 22:38:21 +0200

    asd

--- dist/TVShowsDiscover.55623c88.js
+++ dist/TVShowsDiscover.55623c88.js
@@ -7776,7 +7776,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11238" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12443" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/TVShowsSearch.js
+++ src/js/TVShowsSearch.js
@@ -18,7 +18,7 @@ import "@babel/polyfill";
         let genreOutput = genresArray.map(genre => genre.name).join(", ");
         output +=
           `
-          <div onclick="e(${movie.id})" class="mr-3 card" style="width: 15rem; padding-bottom: 0;">
+          <div onclick="o(${movie.id})" class="mr-3 card" style="width: 15rem; padding-bottom: 0;">
               <img class="card-img-top card-img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title card-small-title">${movie.name}</h5>
@@ -147,7 +147,7 @@ import "@babel/polyfill";
                       </p>
                     </div>
                     <div class="more-info">
-                      <a onclick="e(${movie.id})" target="_blank" class="btn btn-dark">More Details</a>
+                      <a onclick="o(${movie.id})" target="_blank" class="btn btn-dark">More Details</a>
                     </div>
                   </div>
                 </div>
